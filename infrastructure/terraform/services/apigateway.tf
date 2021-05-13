4aaf2e1ed9392c2ff73b7891d991fce62259aba8
wwalpha <wwalpha@gmail.com>
2021-05-13 00:06:08 +0000

    bugfix

--- infrastructure/terraform/services/apigateway.tf
+++ infrastructure/terraform/services/apigateway.tf
@@ -131,126 +131,50 @@ resource "aws_apigatewayv2_integration" "http" {
 # ---------------------------------------------------------------------------------------------
 # API Gateway Route
 # ---------------------------------------------------------------------------------------------
-resource "aws_apigatewayv2_route" "http" {
-  api_id    = aws_apigatewayv2_api.this.id
-  route_key = "ANY /{proxy+}"
-
-  target = "integrations/${aws_apigatewayv2_integration.http[0].id}"
+resource "aws_apigatewayv2_route" "http_post" {
+  api_id             = aws_apigatewayv2_api.this.id
+  route_key          = "POST /{proxy+}"
+  target             = "integrations/${aws_apigatewayv2_integration.http[0].id}"
+  authorizer_id      = aws_apigatewayv2_authorizer.this.id
+  authorization_type = "JWT"
 
   count = local.simple
 }
 
-# # ---------------------------------------------------------------------------------------------
-# # Amazon API REST API
-# # ---------------------------------------------------------------------------------------------
-# module "api" {
-#   source = "github.com/wwalpha/terraform-module-apigateway/api"
+resource "aws_apigatewayv2_route" "http_get" {
+  api_id             = aws_apigatewayv2_api.this.id
+  route_key          = "GET /{proxy+}"
+  target             = "integrations/${aws_apigatewayv2_integration.http[0].id}"
+  authorizer_id      = aws_apigatewayv2_authorizer.this.id
+  authorization_type = "JWT"
 
-#   api_name                   = local.project_name
-#   api_endpoint_configuration = local.api_endpoint_configuration
-#   cognito_user_pool_name     = local.remote_unmu.cognito_user_pool_name
-#   authorizer_name            = "CognitoAuthorizer"
-#   authorizer_type            = "COGNITO_USER_POOLS"
+  count = local.simple
+}
 
-#   # authorizer_role_name             = "${local.project_name_uc}_APIGateway_AuthorizerRole"
-#   # authorizer_policy                = "${file("iam/apigateway_policy_authorizer.json")}"
-#   authorizer_result_ttl_in_seconds = 0
-# }
+resource "aws_apigatewayv2_route" "http_put" {
+  api_id             = aws_apigatewayv2_api.this.id
+  route_key          = "PUT /{proxy+}"
+  target             = "integrations/${aws_apigatewayv2_integration.http[0].id}"
+  authorizer_id      = aws_apigatewayv2_authorizer.this.id
+  authorization_type = "JWT"
 
-# # ---------------------------------------------------------------------------------------------
-# # Amazon API Deployment
-# # ---------------------------------------------------------------------------------------------
-# module "deployment" {
-#   source = "github.com/wwalpha/terraform-module-apigateway/deployment"
+  count = local.simple
+}
 
-#   rest_api_id                            = module.api.id
-#   stage_name                             = "v1"
-#   custom_domain_name                     = aws_acm_certificate.api.domain_name
-#   custom_domain_regional_certificate_arn = aws_acm_certificate_validation.api.certificate_arn
-#   custom_domain_endpoint_configuration   = local.api_endpoint_configuration
+resource "aws_apigatewayv2_route" "http_delete" {
+  api_id             = aws_apigatewayv2_api.this.id
+  route_key          = "DELETE /{proxy+}"
+  target             = "integrations/${aws_apigatewayv2_integration.http[0].id}"
+  authorizer_id      = aws_apigatewayv2_authorizer.this.id
+  authorization_type = "JWT"
 
-#   integration_ids = [
-#     module.m001.integration_id,
-#     module.m002.integration_id,
-#     module.m003.integration_id,
-#     module.m004.integration_id,
-#     module.m005.integration_id,
-#     module.m006.integration_id,
-#     module.m007.integration_id,
-#     module.m008.integration_id,
-#     module.m009.integration_id,
-#     module.m010.integration_id,
-#     module.m011.integration_id,
-#     module.m012.integration_id,
-#     module.m013.integration_id,
-#   ]
+  count = local.simple
+}
 
-#   deployment_md5 = base64encode(join("", local.deployment_files))
-# }
+resource "aws_apigatewayv2_route" "http_options" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "OPTIONS /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.http[0].id}"
 
-# # ----------------------------------------------------------------------------------------------------------------------
-# # Amazon API Gateway Usage Plan
-# # ----------------------------------------------------------------------------------------------------------------------
-# # resource "aws_api_gateway_usage_plan" "usage_plan" {
-# #   name = "ipa_usage_plan"
-
-# #   api_stages {
-# #     api_id = "${aws_api_gateway_rest_api.ipa_api.id}"
-# #     stage  = "${aws_api_gateway_deployment.api_deployment.stage_name}"
-# #   }
-
-# #   quota_settings {
-# #     limit  = 200
-# #     period = "DAY"
-# #   }
-
-# #   throttle_settings {
-# #     burst_limit = 5
-# #     rate_limit  = 5
-# #   }
-# # }
-
-# # resource "aws_api_gateway_api_key" "api_key" {
-# #   name = "ipa_api_key"
-# # }
-
-# # resource "aws_api_gateway_usage_plan_key" "usage_plan_key" {
-# #   key_id        = "${aws_api_gateway_api_key.api_key.id}"
-# #   key_type      = "API_KEY"
-# #   usage_plan_id = "${aws_api_gateway_usage_plan.usage_plan.id}"
-# # }
-# # resource "aws_api_gateway_method_settings" "this" {
-# #   rest_api_id = "${aws_api_gateway_rest_api.this.id}"
-# #   stage_name  = "${aws_api_gateway_stage.this.stage_name}"
-# #   method_path = "*/*"
-
-# #   settings {
-# #     metrics_enabled = false
-# #     logging_level   = "INFO"
-# #   }
-# # }
-
-# # ---------------------------------------------------------------------------------------------
-# # Amazon API Method
-# # ---------------------------------------------------------------------------------------------
-# module "version" {
-#   source = "github.com/wwalpha/terraform-module-apigateway/method"
-
-#   rest_api_id        = module.api.id
-#   resource_id        = module.api.root_resource_id
-#   resource_path      = "/"
-#   http_method        = "GET"
-#   integration_type   = "MOCK"
-#   response_templates = jsonencode(local.response_version)
-# }
-
-# # ---------------------------------------------------------------------------------------------
-# # Amazon API CORS
-# # ---------------------------------------------------------------------------------------------
-# # module "CORS_ROOT" {
-# #   source = "github.com/wwalpha/terraform-module-registry/aws/api-cors"
-# #   rest_api_id = "${aws_api_gateway_rest_api.this.id}"
-# #   resource_id = "${aws_api_gateway_rest_api.this.root_resource_id}"
-# #   allow_origin = "${var.cors_allow_origin}"
-# #   allow_method = "'GET,OPTIONS'"
-# # }
+  count = local.simple
+}
