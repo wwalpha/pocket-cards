925173a55f045de6e6bf83acc1efc75998ee3fcf
wwalpha <wwalpha@gmail.com>
2021-07-29 00:08:30 +0000

    add status

---
+++ backend/nodejs/lambda/status/src/app.ts
@@ -0,0 +1,81 @@
+import { ECS, EC2, ApiGatewayV2 } from 'aws-sdk';
+
+const CLUSTER_ARN = process.env.CLUSTER_ARN;
+const API_ID = process.env.API_ID;
+const INTEGRATION_ID = process.env.INTEGRATION_ID;
+
+export default async () => {
+  const ecs = new ECS();
+  const ec2 = new EC2();
+
+  // list tasks
+  const tasks = await ecs
+    .listTasks({
+      cluster: CLUSTER_ARN,
+    })
+    .promise();
+
+  // list numbers check
+  if (!tasks.taskArns || tasks.taskArns.length === 0) {
+    return {
+      status: 'STOPPED',
+    };
+  }
+
+  const details = await ecs
+    .describeTasks({
+      cluster: CLUSTER_ARN,
+      tasks: tasks.taskArns,
+    })
+    .promise();
+
+  if (!details.tasks || details.tasks.length === 0) {
+    return {
+      status: 'STOPPED',
+    };
+  }
+
+  const task = details.tasks[0];
+
+  return {
+    status: task.lastStatus,
+  };
+
+  // if (task.lastStatus !== 'RUNNING') {
+  //   return {
+  //     status: task.lastStatus,
+  //   };
+  // }
+
+  // // find eni infomation
+  // const enis = task.attachments.map((item) => {
+  //   const f = item.details.find((i) => i.name === 'networkInterfaceId');
+
+  //   return f.value;
+  // });
+
+  // // get eni details
+  // const eniDetails = await ec2
+  //   .describeNetworkInterfaces({
+  //     NetworkInterfaceIds: enis,
+  //   })
+  //   .promise();
+
+  // // get public ip
+  // const publicIp = eniDetails.NetworkInterfaces[0].Association.PublicIp;
+
+  // const api = new ApiGatewayV2();
+
+  // // update integration target ip
+  // await api
+  //   .updateIntegration({
+  //     ApiId: API_ID,
+  //     IntegrationId: INTEGRATION_ID,
+  //     IntegrationUri: `http://${publicIp}/{proxy}`,
+  //   })
+  //   .promise();
+
+  // return {
+  //   status: task.lastStatus,
+  // };
+};
