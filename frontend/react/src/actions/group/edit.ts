aede34ca476af293d9d5ed5a8d49aaa80430f37a
wwalpha <wwalpha@gmail.com>
2021-08-22 08:33:59 +0000

    update

--- frontend/react/src/actions/group/edit.ts
+++ frontend/react/src/actions/group/edit.ts
@@ -1,14 +1,11 @@
 import { push } from 'connected-react-router';
-import { createAction } from 'redux-actions';
-import { defaultFailure, startLoading } from '@actions';
-import { ActionTypes, Consts, Paths } from '@constants';
+import { defaultFailure, endLoading, startLoading } from '@actions';
+import { Consts, Paths } from '@constants';
 import { Actions, APIs, App } from 'typings';
 import { list } from './list';
 
-const success = createAction(ActionTypes.E0_03_SUCCESS);
-
 /** グループ編集 */
-export const edit: Actions.GroupEditAction = (info: App.GroupInfo) => async (dispatch, store, api) => {
+export const edit: Actions.GroupEditAction = (info: App.GroupInfo) => async (dispatch, _, api) => {
   // 開始イベント
   dispatch(startLoading());
 
@@ -23,9 +20,9 @@ export const edit: Actions.GroupEditAction = (info: App.GroupInfo) => async (dis
     dispatch(list());
     // グループリスト画面に遷移する
     dispatch(push(Paths.ROUTE_PATHS[Paths.ROUTE_PATH_INDEX.Groups]));
-    // 正常終了
-    dispatch(success());
   } catch (err) {
     dispatch(defaultFailure(err));
+  } finally {
+    dispatch(endLoading());
   }
 };
