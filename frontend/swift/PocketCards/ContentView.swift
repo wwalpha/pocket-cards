956203fe2cb271ad323546f4604f9f81c9995c0a
wwalpha <wwalpha@gmail.com>
2022-03-07 09:17:13 +0900

    add swift

---
+++ frontend/swift/PocketCards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PocketCards
+//
+//  Created by macmini on 2022/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ frontend/swift/PocketCards/PocketCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketCardsApp.swift
+//  PocketCards
+//
+//  Created by macmini on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct PocketCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
