c961961da575078dba0ac4a87007cfc7c3b058c1
wwalpha <wwalpha@gmail.com>
2022-03-14 03:00:48 +0000

    add origin

--- infrastructure/terraform/services/cloudfront.tf
+++ infrastructure/terraform/services/cloudfront.tf
@@ -49,7 +49,7 @@ resource "aws_cloudfront_distribution" "this" {
   }
 
   ordered_cache_behavior {
-    path_pattern     = "/contents/*"
+    path_pattern     = "/images/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
     target_origin_id = local.origin_id_materials
@@ -65,6 +65,25 @@ resource "aws_cloudfront_distribution" "this" {
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
   }
+
+  ordered_cache_behavior {
+    path_pattern     = "/voices/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.origin_id_materials
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+  }
+
 
   viewer_certificate {
     cloudfront_default_certificate = false
