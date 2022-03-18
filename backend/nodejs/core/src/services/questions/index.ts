bda9257a643ec6a22fbfdd68ec90f0d6b49adb7a
wwalpha <wwalpha@gmail.com>
2022-03-18 23:56:18 +0000

    rename

--- backend/nodejs/core/src/services/questions/index.ts
+++ backend/nodejs/core/src/services/questions/index.ts
@@ -1,4 +1,4 @@
 export { default as QuestionRegist } from './regist';
 export { default as QuestionStudy } from './study';
-export { default as QuestionTest } from './test';
+export { default as QuestionExam } from './exam';
 export { default as QuestionAnswer } from './answer';
