4f5f0c36c423fc0201034a20f3f1feb5302ccc14
wwalpha <wwalpha@gmail.com>
2022-03-21 13:27:59 +0000

    履歴登録

--- backend/nodejs/core/src/services/questions/answer.ts
+++ backend/nodejs/core/src/services/questions/answer.ts
@@ -1,7 +1,7 @@
 import { Request } from 'express';
 import { defaultTo } from 'lodash';
 import { DateUtils, DBHelper } from '@utils';
-import { Learning } from '@queries';
+import { Histories, Learning } from '@queries';
 import { APIs, Tables } from 'typings';
 
 export default async (
@@ -21,12 +21,30 @@ export default async (
   const times = input.correct === '1' ? defaultTo(question.times, 0) + 1 : 0;
   const nextTime = input.correct === '1' ? DateUtils.getNextTime(times) : DateUtils.getNextTime(0);
 
-  // 問題情報更新
-  await DBHelper().put(
-    Learning.put({
-      ...question,
-      times: times,
-      nextTime: nextTime,
-    })
-  );
+  // 登録実行
+  await DBHelper().transactWrite({
+    TransactItems: [
+      {
+        // 問題情報更新
+        Put: Learning.put({
+          ...question,
+          times: times,
+          nextTime: nextTime,
+        }),
+      },
+      {
+        // 履歴登録
+        Put: Histories.put({
+          qid: question.qid,
+          timestamp: DateUtils.getTimestamp(),
+          groupId: question.groupId,
+          userId: question.userId,
+          subject: question.subject,
+          timesBefore: question.times,
+          timesAfter: times,
+          lastTime: question.lastTime,
+        }),
+      },
+    ],
+  });
 };
