ac1d41f7dcf954eabb32b42da5bdd3e2d7b8d759
wwalpha <wwalpha@gmail.com>
2022-03-30 08:52:15 +0900

    remove routes

--- infrastructure/terraform/services/apigw_routes.tf
+++ infrastructure/terraform/services/apigw_routes.tf
@@ -1,26 +1,35 @@
-# ---------------------------------------------------------------------------------------------
-# API Gateway Route - Auth Health
-# ---------------------------------------------------------------------------------------------
-resource "aws_apigatewayv2_route" "get_auth_health" {
-  api_id    = aws_apigatewayv2_api.this.id
-  route_key = "GET /auth/health"
-  target    = "integrations/${aws_apigatewayv2_integration.auth.id}"
-}
+# # ---------------------------------------------------------------------------------------------
+# # API Gateway Route - Auth Health
+# # ---------------------------------------------------------------------------------------------
+# resource "aws_apigatewayv2_route" "get_auth_health" {
+#   api_id    = aws_apigatewayv2_api.this.id
+#   route_key = "GET /auth/health"
+#   target    = "integrations/${aws_apigatewayv2_integration.auth.id}"
+# }
 
-# ---------------------------------------------------------------------------------------------
-# API Gateway Route - Auth
-# ---------------------------------------------------------------------------------------------
-resource "aws_apigatewayv2_route" "post_auth" {
-  api_id    = aws_apigatewayv2_api.this.id
-  route_key = "POST /auth/login"
-  target    = "integrations/${aws_apigatewayv2_integration.auth.id}"
-}
+# # ---------------------------------------------------------------------------------------------
+# # API Gateway Route - Auth
+# # ---------------------------------------------------------------------------------------------
+# resource "aws_apigatewayv2_route" "post_auth" {
+#   api_id    = aws_apigatewayv2_api.this.id
+#   route_key = "POST /auth/login"
+#   target    = "integrations/${aws_apigatewayv2_integration.auth.id}"
+# }
 
-# ---------------------------------------------------------------------------------------------
-# API Gateway Route - Auth Token Refresh
-# ---------------------------------------------------------------------------------------------
-resource "aws_apigatewayv2_route" "post_auth_refresh" {
-  api_id    = aws_apigatewayv2_api.this.id
-  route_key = "POST /auth/refresh"
-  target    = "integrations/${aws_apigatewayv2_integration.auth.id}"
-}
+# # ---------------------------------------------------------------------------------------------
+# # API Gateway Route - Auth Token Refresh
+# # ---------------------------------------------------------------------------------------------
+# resource "aws_apigatewayv2_route" "post_auth_refresh" {
+#   api_id    = aws_apigatewayv2_api.this.id
+#   route_key = "POST /auth/refresh"
+#   target    = "integrations/${aws_apigatewayv2_integration.auth.id}"
+# }
+
+# # ---------------------------------------------------------------------------------------------
+# # API Gateway Route - Group Functions
+# # ---------------------------------------------------------------------------------------------
+# resource "aws_apigatewayv2_route" "post_group_func" {
+#   api_id    = aws_apigatewayv2_api.this.id
+#   route_key = "POST /groups/{proxy+}"
+#   target    = "integrations/${aws_apigatewayv2_integration.backend.id}"
+# }
