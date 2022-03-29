17590bc491ab327ac7af696271a3e5df7210ada9
wwalpha <wwalpha@gmail.com>
2022-03-29 20:55:36 +0900

    update

--- infrastructure/terraform/services/ecs_auth.tf
+++ infrastructure/terraform/services/ecs_auth.tf
@@ -4,7 +4,7 @@
 resource "aws_ecs_service" "auth" {
   depends_on = [aws_ecs_cluster.this]
 
-  name                               = "auth"
+  name                               = "auth_manager"
   cluster                            = aws_ecs_cluster.this.id
   desired_count                      = 0
   platform_version                   = "LATEST"
