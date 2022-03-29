d041b61e0bbcc19d6c870dd944e8844f35b3891f
wwalpha <wwalpha@gmail.com>
2022-03-29 18:54:04 +0900

    add typedef

---
+++ backend/nodejs/typings/auth.d.ts
@@ -0,0 +1,38 @@
+export namespace Auth {
+  /** User Login Request */
+  interface SignInRequest {
+    username: string;
+    password: string;
+    mfaCode?: string;
+    newPassword?: string;
+  }
+
+  /** User Login Response */
+  interface SignInResponse extends AuthenticateFailure {
+    idToken?: string;
+    accessToken?: string;
+    refreshToken?: string;
+  }
+
+  interface UserPoolInfo {
+    clientId: string;
+    userPoolId: string;
+  }
+
+  interface AuthenticateFailure {
+    newPasswordRequired?: boolean;
+    mfaRequired?: boolean;
+  }
+
+  /** Get new credentials with refresh token request */
+  interface InitiateAuthRequest {
+    accessToken: string;
+    refreshToken: string;
+  }
+
+  /** Get new credentials with refresh token response */
+  interface InitiateAuthResponse {
+    idToken: string;
+    accessToken: string;
+  }
+}
--- backend/nodejs/typings/index.d.ts
+++ backend/nodejs/typings/index.d.ts
@@ -1,3 +1,5 @@
 export * from './tables';
 export * from './types';
 export * from './api';
+export * from './auth';
+export * from './users';
