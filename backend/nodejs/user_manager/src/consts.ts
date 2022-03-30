0af6024fb79d2f071b48fedb415694e9424f24b9
wwalpha <wwalpha@gmail.com>
2022-03-30 19:17:03 +0900

    update

--- backend/nodejs/user_manager/src/consts.ts
+++ backend/nodejs/user_manager/src/consts.ts
@@ -2,5 +2,5 @@ export const Environments = {
   AWS_REGION: process.env.AWS_REGION as string,
   AWS_ENDPOINT: process.env.AWS_ENDPOINT as string,
   TABLE_NAME_SETTINGS: process.env.TABLE_NAME_SETTINGS as string,
-  TABLE_NAME_USER: process.env.TABLE_NAME_USER as string,
+  TABLE_NAME_USERS: process.env.TABLE_NAME_USERS as string,
 };
