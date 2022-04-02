df82088e2c51d23aca9ad7e39b2c668136cb7963
wwalpha <wwalpha@gmail.com>
2022-04-02 15:02:41 +0900

    update files

--- backend/terraform/lambda.tf
+++ backend/terraform/lambda.tf
@@ -2,8 +2,9 @@
 # Lambda Function - ECS Task Start
 # ----------------------------------------------------------------------------------------------
 resource "aws_lambda_function" "ecs_task_start" {
-  filename         = data.archive_file.ecs_task_start.output_path
-  source_code_hash = data.archive_file.ecs_task_start.output_base64sha256
+  s3_bucket        = local.bucket_name_archive
+  s3_key           = "lambda/start.zip"
+  source_code_hash = aws_s3_object.lambda_start.etag
   function_name    = "${local.project_name}-ecs-task-start"
   handler          = local.lambda_handler
   memory_size      = 128
@@ -20,12 +21,6 @@ resource "aws_lambda_function" "ecs_task_start" {
   }
 }
 
-data "archive_file" "ecs_task_start" {
-  type        = "zip"
-  source_dir  = "../nodejs/lambda/start/dist"
-  output_path = "../nodejs/lambda/start/dist.zip"
-}
-
 # ---------------------------------------------------------------------------------------------
 # API Gateway Permission (Lambda) - ECS Task Start
 # ---------------------------------------------------------------------------------------------
@@ -40,8 +35,9 @@ resource "aws_lambda_permission" "ecs_task_start" {
 # Lambda Function - ECS Task Stop
 # ----------------------------------------------------------------------------------------------
 resource "aws_lambda_function" "ecs_task_stop" {
-  filename         = data.archive_file.ecs_task_stop.output_path
-  source_code_hash = data.archive_file.ecs_task_stop.output_base64sha256
+  s3_bucket        = local.bucket_name_archive
+  s3_key           = "lambda/stop.zip"
+  source_code_hash = aws_s3_object.lambda_stop.etag
   function_name    = "${local.project_name}-ecs-task-stop"
   handler          = local.lambda_handler
   memory_size      = 128
@@ -58,12 +54,6 @@ resource "aws_lambda_function" "ecs_task_stop" {
   }
 }
 
-data "archive_file" "ecs_task_stop" {
-  type        = "zip"
-  source_dir  = "../nodejs/lambda/stop/dist"
-  output_path = "../nodejs/lambda/stop/dist.zip"
-}
-
 # ---------------------------------------------------------------------------------------------
 # API Gateway Permission (Lambda) - ECS Task Stop
 # ---------------------------------------------------------------------------------------------
@@ -78,8 +68,9 @@ resource "aws_lambda_permission" "ecs_task_stop" {
 # Lambda Function - ECS Task Status
 # ----------------------------------------------------------------------------------------------
 resource "aws_lambda_function" "ecs_task_status" {
-  filename         = data.archive_file.ecs_task_status.output_path
-  source_code_hash = data.archive_file.ecs_task_status.output_base64sha256
+  s3_bucket        = local.bucket_name_archive
+  s3_key           = "lambda/status.zip"
+  source_code_hash = aws_s3_object.lambda_status.etag
   function_name    = "${local.project_name}-ecs-task-status"
   handler          = local.lambda_handler
   memory_size      = 128
@@ -91,12 +82,6 @@ resource "aws_lambda_function" "ecs_task_status" {
       CLUSTER_ARN = data.aws_ecs_cluster.this.arn
     }
   }
-}
-
-data "archive_file" "ecs_task_status" {
-  type        = "zip"
-  source_dir  = "../nodejs/lambda/status/dist"
-  output_path = "../nodejs/lambda/status/dist.zip"
 }
 
 # ---------------------------------------------------------------------------------------------
@@ -127,35 +112,5 @@ resource "aws_lambda_function" "batch" {
       TABLE_NAME_HISTORIES = local.dynamodb_name_histories
       TZ                   = "Asia/Tokyo"
     }
-  }
-}
-
-# ----------------------------------------------------------------------------------------------
-# Lambda Deploy - Cognito post signup
-# ----------------------------------------------------------------------------------------------
-data "archive_file" "cognito_post_signup" {
-  type        = "zip"
-  source_dir  = "../nodejs/lambda/cognito/dist"
-  output_path = "../nodejs/lambda/cognito/dist.zip"
-}
-
-resource "null_resource" "cognito_post_signup" {
-  provisioner "local-exec" {
-    command = "aws lambda update-function-code --function-name ${local.lambda_function_name_cognito_post_signup} --zip-file fileb://${data.archive_file.cognito_post_signup.output_path}"
-  }
-}
-
-# ----------------------------------------------------------------------------------------------
-# Lambda Deploy - API Gateway Authroizer
-# ----------------------------------------------------------------------------------------------
-data "archive_file" "authorizer" {
-  type        = "zip"
-  source_dir  = "../nodejs/lambda/authorizer/dist"
-  output_path = "../nodejs/lambda/authorizer/dist.zip"
-}
-
-resource "null_resource" "authorizer" {
-  provisioner "local-exec" {
-    command = "aws lambda update-function-code --function-name ${local.lambda_function_name_authorizer} --zip-file fileb://${data.archive_file.authorizer.output_path}"
   }
 }
