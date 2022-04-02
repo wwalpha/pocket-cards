531a73be58dcd261dcafab1124e1ad0e9836f1b6
wwalpha <wwalpha@gmail.com>
2022-04-02 12:42:31 +0900

    add authorizer function

---
+++ backend/nodejs/authorizer/src/consts.ts
@@ -0,0 +1,4 @@
+export const Environments = {
+  TABLE_NAME_USERS: process.env.TABLE_NAME_USERS as string,
+  TABLE_NAME_SETTINGS: process.env.TABLE_NAME_SETTINGS as string,
+};
---
+++ backend/nodejs/authorizer/src/index.ts
@@ -0,0 +1,145 @@
+import { APIGatewayAuthorizerResult, APIGatewayRequestAuthorizerEventV2 } from 'aws-lambda';
+import { JwtPayload } from 'jsonwebtoken';
+import winston from 'winston';
+import { decodeToken, getPublicKeys, validateToken } from './utils';
+import { Environments } from './consts';
+import { ApiOptions, AuthPolicy } from './AuthPolicy';
+import { DynamoDB } from 'aws-sdk';
+import { Tables } from '../../typings/tables';
+
+const PEM_KEYS: Record<string, Record<string, string>> = {};
+const API_KEYS: string[] = [];
+const Logger = winston.createLogger({
+  level: 'info',
+  defaultMeta: { service: 'authorizer' },
+  transports: [new winston.transports.Console()],
+});
+
+const client = new DynamoDB.DocumentClient({
+  region: process.env.AWS_DEFAULT_REGION,
+});
+
+export const handler = async (event: APIGatewayRequestAuthorizerEventV2): Promise<APIGatewayAuthorizerResult> => {
+  Logger.info('event', event);
+
+  // authorizator token
+  const identitySource = event.identitySource[0];
+
+  // if (event.headers['x-api-key']) {
+  //   if (API_KEYS.length === 0) {
+  //     const result = await helper.get<Tables.Settings.APIKey>({
+  //       TableName: Environments.TABLE_NAME_SETTINGS,
+  //       Key: {
+  //         Id: 'API_KEY',
+  //       } as Tables.Settings.Key,
+  //     });
+
+  //     result?.Item?.Keys.forEach((item) => API_KEYS.push(item));
+  //   }
+
+  //   return { isAuthorized: API_KEYS.includes(identitySource) };
+  // }
+
+  try {
+    const decodedToken = decodeToken(identitySource);
+
+    const token = decodedToken;
+    const payload = token.payload as JwtPayload;
+    const iss = payload.iss;
+    const kid = token.header.kid;
+
+    // iss not exist
+    if (!iss || !kid) {
+      return authorizationFailure();
+    }
+
+    let pems: Record<string, string> | undefined = PEM_KEYS[iss];
+
+    // not cached
+    if (!pems) {
+      // cache public keys
+      PEM_KEYS[iss] = await getPublicKeys(iss);
+    }
+
+    // principalId
+    const principalId = payload.email;
+
+    return await buildAuthPolicy(event, principalId);
+  } catch (err) {
+    console.log(err);
+
+    return authorizationFailure();
+  }
+};
+
+/**
+ * Build IAM Policy
+ *
+ * @param event event
+ * @param principalId user id
+ * @returns
+ */
+const buildAuthPolicy = async (
+  event: APIGatewayRequestAuthorizerEventV2,
+  principalId: string
+): Promise<APIGatewayAuthorizerResult> => {
+  const apiOptions: ApiOptions = {};
+  const infos = event.routeArn.split(':');
+  const region = infos[3];
+  const { accountId, apiId, stage } = event.requestContext;
+
+  apiOptions.region = region;
+  apiOptions.restApiId = apiId;
+  apiOptions.stage = stage;
+
+  const policy = new AuthPolicy(principalId, accountId, apiOptions);
+  const status = await getUserRole(principalId);
+
+  switch (status) {
+    case 'TENANT_ADMIN':
+      policy.allowMethod(AuthPolicy.HttpVerb.ALL, '/admin/*');
+      break;
+    case 'PARENT':
+      policy.denyMethod(AuthPolicy.HttpVerb.ALL, '/admin/*');
+      policy.allowAllMethods();
+      break;
+    case 'CHILD':
+      policy.denyMethod(AuthPolicy.HttpVerb.ALL, '/admin/*');
+      policy.allowAllMethods();
+      break;
+    default:
+      policy.denyAllMethods();
+  }
+
+  return policy.build();
+};
+
+const getUserRole = async (userId: string): Promise<string> => {
+  // get user role from db
+  const result = await client
+    .get({
+      TableName: Environments.TABLE_NAME_USERS,
+      Key: {
+        id: userId,
+      } as Tables.TUsersKey,
+    })
+    .promise();
+
+  const userInfo = result.Item;
+
+  // user not found
+  if (!userInfo) return '';
+
+  return (userInfo as Tables.TUsers).status;
+};
+
+/**
+ * Authorize failure
+ * @returns
+ */
+const authorizationFailure = (): APIGatewayAuthorizerResult => {
+  const policy = new AuthPolicy('*', '*');
+  policy.denyAllMethods();
+
+  return policy.build();
+};
