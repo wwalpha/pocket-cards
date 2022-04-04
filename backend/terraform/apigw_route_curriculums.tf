f2bd1042240de0e808ce1bd01c873e4de21bcd83
wwalpha <wwalpha@gmail.com>
2022-04-04 15:01:41 +0900

    update

--- backend/terraform/apigw_route_curriculums.tf
+++ backend/terraform/apigw_route_curriculums.tf
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------------------------------------------
 resource "aws_apigatewayv2_route" "get_curriculums_proxy" {
   api_id             = local.apigw_id
-  route_key          = "ANY /curriculums/{proxy+}"
+  route_key          = "DELETE /curriculums/{proxy+}"
   target             = "integrations/${local.apigw_integration_id_backend}"
   authorization_type = "CUSTOM"
   authorizer_id      = local.apigw_authorizer_id_lambda
