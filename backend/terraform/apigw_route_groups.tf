99192f5ff82c80bf0c6f59613260fce6e4058c46
wwalpha <wwalpha@gmail.com>
2022-04-04 11:09:03 +0900

    remove path

--- backend/terraform/apigw_route_groups.tf
+++ backend/terraform/apigw_route_groups.tf
@@ -23,15 +23,6 @@ resource "aws_apigatewayv2_route" "put_groups" {
 # ---------------------------------------------------------------------------------------------
 # API Gateway Route - Group Functions
 # ---------------------------------------------------------------------------------------------
-resource "aws_apigatewayv2_route" "opts_group" {
-  api_id    = local.apigw_id
-  route_key = "OPTIONS /groups"
-  target    = "integrations/${local.apigw_integration_id_backend}"
-}
-
-# ---------------------------------------------------------------------------------------------
-# API Gateway Route - Group Functions
-# ---------------------------------------------------------------------------------------------
 resource "aws_apigatewayv2_route" "post_group_func" {
   api_id             = local.apigw_id
   route_key          = "POST /groups/{proxy+}"
@@ -71,13 +62,4 @@ resource "aws_apigatewayv2_route" "del_group_func" {
   target             = "integrations/${local.apigw_integration_id_backend}"
   authorization_type = "CUSTOM"
   authorizer_id      = local.apigw_authorizer_id_lambda
-}
-
-# ---------------------------------------------------------------------------------------------
-# API Gateway Route - Group Functions
-# ---------------------------------------------------------------------------------------------
-resource "aws_apigatewayv2_route" "opts_group_func" {
-  api_id    = local.apigw_id
-  route_key = "OPTIONS /groups/{proxy+}"
-  target    = "integrations/${local.apigw_integration_id_backend}"
 }
