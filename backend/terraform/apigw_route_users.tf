e87a056b687709a1703ea050fe04b5ac25f54f32
wwalpha <wwalpha@gmail.com>
2022-04-07 15:07:46 +0900

    add new apis

--- backend/terraform/apigw_route_users.tf
+++ backend/terraform/apigw_route_users.tf
@@ -8,10 +8,32 @@ resource "aws_apigatewayv2_route" "get_users_health" {
 }
 
 # ---------------------------------------------------------------------------------------------
-# API Gateway Route - Auth
+# API Gateway Route - Create Normal User
 # ---------------------------------------------------------------------------------------------
 resource "aws_apigatewayv2_route" "post_users" {
   api_id    = local.apigw_id
   route_key = "POST /users"
   target    = "integrations/${local.apigw_integration_id_users}"
+}
+
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - Create Student
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "post_users_students" {
+  api_id             = local.apigw_id
+  route_key          = "POST /users/students"
+  target             = "integrations/${local.apigw_integration_id_users}"
+  authorization_type = "CUSTOM"
+  authorizer_id      = local.apigw_authorizer_id_lambda
+}
+
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - Get students by teacher
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "get_users_students" {
+  api_id             = local.apigw_id
+  route_key          = "GET /users/students"
+  target             = "integrations/${local.apigw_integration_id_users}"
+  authorization_type = "CUSTOM"
+  authorizer_id      = local.apigw_authorizer_id_lambda
 }
