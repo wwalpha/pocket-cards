6fe7addc12659df16db91c23959f804211d40a6b
wwalpha <wwalpha@gmail.com>
2022-04-10 09:51:50 +0900

    add new route

--- backend/terraform/apigw_route_users.tf
+++ backend/terraform/apigw_route_users.tf
@@ -37,3 +37,14 @@ resource "aws_apigatewayv2_route" "get_users_students" {
   authorization_type = "CUSTOM"
   authorizer_id      = local.apigw_authorizer_id_lambda
 }
+
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - Get user infomations
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "get_users_proxy" {
+  api_id             = local.apigw_id
+  route_key          = "GET /users/{proxy+}"
+  target             = "integrations/${local.apigw_integration_id_users}"
+  authorization_type = "CUSTOM"
+  authorizer_id      = local.apigw_authorizer_id_lambda
+}
