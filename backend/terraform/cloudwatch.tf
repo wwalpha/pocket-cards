a1def2c4a516a09a6871c76844b86e1b1220ed05
wwalpha <wwalpha@gmail.com>
2022-04-15 13:52:58 +0900

    update

--- backend/terraform/cloudwatch.tf
+++ backend/terraform/cloudwatch.tf
@@ -93,28 +93,27 @@ resource "aws_lambda_permission" "this" {
 resource "aws_cloudwatch_event_rule" "startat_2000" {
   name                = "${local.project_name}-ecs-start-at-1600"
   schedule_expression = "cron(0 11 * * ? *)"
-  count               = local.prod_only
 }
 
 # ----------------------------------------------------------------------------------------------
 # AWS CloudWatch Event Rule Target - Start at 20:00 
 # ----------------------------------------------------------------------------------------------
 resource "aws_cloudwatch_event_target" "startat_2000" {
-  rule      = aws_cloudwatch_event_rule.startat_2000[0].name
-  target_id = "SendToLambda"
-  arn       = aws_lambda_function.ecs_task_start.arn
-  count     = local.prod_only
+  depends_on = [aws_cloudwatch_event_rule.startat_2000]
+  rule       = aws_cloudwatch_event_rule.startat_2000.name
+  target_id  = "SendToLambda"
+  arn        = aws_lambda_function.ecs_task_start.arn
 }
 
 # ---------------------------------------------------------------------------------------------
 # AWS CloudWatch Event Rule Permission - Start at 20:00 
 # ---------------------------------------------------------------------------------------------
 resource "aws_lambda_permission" "startat_2000" {
+  depends_on    = [aws_cloudwatch_event_target.startat_2000]
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.ecs_task_start.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.startat_2000[0].arn
-  count         = local.prod_only
+  source_arn    = aws_cloudwatch_event_rule.startat_2000.arn
 }
 
 # ----------------------------------------------------------------------------------------------
@@ -123,17 +122,15 @@ resource "aws_lambda_permission" "startat_2000" {
 resource "aws_cloudwatch_event_rule" "stopat_2400" {
   name                = "${local.project_name}-ecs-stop-at-2400"
   schedule_expression = "cron(0 15 * * ? *)"
-  count               = local.prod_only
 }
 
 # ----------------------------------------------------------------------------------------------
 # AWS CloudWatch Event Rule Target - Stop at 24:00 
 # ----------------------------------------------------------------------------------------------
 resource "aws_cloudwatch_event_target" "stopat_2400" {
-  rule      = aws_cloudwatch_event_rule.stopat_2400[0].name
+  rule      = aws_cloudwatch_event_rule.stopat_2400.name
   target_id = "SendToLambda"
   arn       = aws_lambda_function.ecs_task_stop.arn
-  count     = local.prod_only
 }
 
 # ---------------------------------------------------------------------------------------------
@@ -143,7 +140,5 @@ resource "aws_lambda_permission" "stopat_2400" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.ecs_task_stop.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.stopat_2400[0].arn
-  count         = local.prod_only
+  source_arn    = aws_cloudwatch_event_rule.stopat_2400.arn
 }
-
