ac5c43b4c1ca0f3b301fa5e2398e0dd83db55b87
wwalpha <wwalpha@gmail.com>
2022-04-25 11:41:27 +0900

    add patch

--- backend/nodejs/core/src/services/patch/patch.ts
+++ backend/nodejs/core/src/services/patch/patch.ts
@@ -1,1 +1,20 @@
-export default async (): Promise<void> => {};
+import { Environment } from '@consts';
+import { DBHelper } from '@utils';
+import { Tables } from 'typings';
+
+export default async (): Promise<void> => {
+  const curriculums = await DBHelper().scan<Tables.TCurriculums>({
+    TableName: Environment.TABLE_NAME_CURRICULUMS,
+  });
+  const groups = await DBHelper().scan<Tables.TGroups>({
+    TableName: Environment.TABLE_NAME_GROUPS,
+  });
+
+  const newRecords = curriculums.Items.map<Tables.TCurriculums>((item) => ({
+    ...item,
+    subject: groups.Items.find((group) => group.id === item.groupId)?.subject || '',
+  }));
+
+  // reset all records
+  await DBHelper().bulk(Environment.TABLE_NAME_CURRICULUMS, newRecords);
+};
