7387c3d4e4e38921a748bc04b08b5c8629f3b959
wwalpha <wwalpha@gmail.com>
2022-04-26 21:57:34 +0900

    update

--- backend/nodejs/core/src/services/patch/patch.ts
+++ backend/nodejs/core/src/services/patch/patch.ts
@@ -1,4 +1,5 @@
 import { Environment } from '@consts';
+import { Questions } from '@queries';
 import { DBHelper } from '@utils';
 import { Tables } from 'typings';
 
@@ -17,4 +18,19 @@ export default async (): Promise<void> => {
 
   // reset all records
   await DBHelper().bulk(Environment.TABLE_NAME_CURRICULUMS, newRecords);
+
+  const questions = await DBHelper().scan<Tables.TQuestions>({
+    TableName: Environment.TABLE_NAME_QUESTIONS,
+  });
+
+  const tasks = questions.Items.map((item) => {
+    DBHelper().put(
+      Questions.put({
+        ...item,
+        subject: groups.Items.find((g) => g.id === item.groupId)?.subject || '',
+      })
+    );
+  });
+
+  await Promise.all(tasks);
 };
