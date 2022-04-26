d31da188a14c5e3e115fc0dc64f07ce21458f3ef
wwalpha <wwalpha@gmail.com>
2022-04-26 23:06:20 +0900

    update

--- backend/terraform/apigw_route_questions.tf
+++ backend/terraform/apigw_route_questions.tf
@@ -19,3 +19,14 @@ resource "aws_apigatewayv2_route" "post_questions" {
   authorization_type = "CUSTOM"
   authorizer_id      = local.apigw_authorizer_id_lambda
 }
+
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - PUT Questions
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "put_questions" {
+  api_id             = local.apigw_id
+  route_key          = "PUT /questions/{proxy+}"
+  target             = "integrations/${local.apigw_integration_id_backend}"
+  authorization_type = "CUSTOM"
+  authorizer_id      = local.apigw_authorizer_id_lambda
+}
