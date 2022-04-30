4dd9447933bcd23f49b53caff769457a46ead2a2
wwalpha <wwalpha@gmail.com>
2022-04-30 19:57:01 +0900

    update

---
+++ frontend/swift/PocketCards/Components/Checks/CheckList.swift
@@ -0,0 +1,72 @@
+//
+//  CheckList.swift
+//  PocketCards
+//
+//  Created by macmini on 2022/04/30.
+//
+
+import SwiftUI
+
+struct CheckListItem {
+    private var id = UUID()
+    var key: String
+    var name: String
+    var isChecked: Bool = false
+
+    init(key: String, name: String) {
+        self.key = key
+        self.name = name
+    }
+}
+
+struct CheckList: View {
+    @State var datas: [CheckListItem] = []
+    @Binding var selection: Set<String>
+
+    var body: some View {
+        List {
+            ForEach(0 ..< datas.count, id: \.self) { index in
+                HStack {
+                    Image(systemName: datas[index].isChecked ? "checkmark.square.fill" : "square")
+                        .onTapGesture {
+                            datas[index].isChecked.toggle()
+
+                            debugPrint(1111, selection)
+                            if datas[index].isChecked {
+                                debugPrint(4444, selection)
+                                selection.insert(datas[index].key)
+                            } else {
+                                debugPrint(5555, selection)
+                                selection.remove(datas[index].key)
+                            }
+                            debugPrint(2222, selection)
+                        }
+                    Text(datas[index].name)
+                        .onTapGesture {
+                            datas[index].isChecked.toggle()
+
+                            if datas[index].isChecked {
+                                selection.insert(datas[index].key)
+                            } else {
+                                selection.remove(datas[index].key)
+                            }
+                        }
+                }
+            }
+        }
+    }
+}
+
+// struct CheckList_Previews: PreviewProvider {
+//    @State var selection = Set<String> ()
+//
+//    static var previews: some View {
+//        var datas: [CheckListItem] = []
+//        datas.append(CheckListItem(key: "AAA", name: "Name1"))
+//        datas.append(CheckListItem(key: "BBB", name: "Name2"))
+//        datas.append(CheckListItem(key: "CCC", name: "Name3"))
+//
+//
+//        return CheckList(datas: datas, selection: $selection)
+//    }
+// }
