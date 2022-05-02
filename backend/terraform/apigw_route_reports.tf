96008a1ad7af99c407461b47a54cedfbac5d2fdb
wwalpha <wwalpha@gmail.com>
2022-05-02 09:25:10 +0900

    update

--- backend/terraform/apigw_route_reports.tf
+++ backend/terraform/apigw_route_reports.tf
@@ -9,6 +9,17 @@ resource "aws_apigatewayv2_route" "get_reports" {
   authorizer_id      = local.apigw_authorizer_id_lambda
 }
 
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - Post Reports
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "post_reports" {
+  api_id             = local.apigw_id
+  route_key          = "POST /reports/{proxy+}"
+  target             = "integrations/${local.apigw_integration_id_backend}"
+  authorization_type = "CUSTOM"
+  authorizer_id      = local.apigw_authorizer_id_lambda
+}
+
 resource "aws_apigatewayv2_route" "patch" {
   api_id    = local.apigw_id
   route_key = "PATCH /patch"
