3169a07b6a63f8b2cd282641c6038f8475331b92
wwalpha <wwalpha@gmail.com>
2022-05-09 22:08:42 +0900

    add new route

--- backend/terraform/apigw_route_study.tf
+++ backend/terraform/apigw_route_study.tf
@@ -19,3 +19,25 @@ resource "aws_apigatewayv2_route" "post_weekly" {
   authorization_type = "CUSTOM"
   authorizer_id      = local.apigw_authorizer_id_lambda
 }
+
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - Study Path
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "get_study" {
+  api_id             = local.apigw_id
+  route_key          = "GET /study/{proxy+}"
+  target             = "integrations/${local.apigw_integration_id_backend}"
+  authorization_type = "CUSTOM"
+  authorizer_id      = local.apigw_authorizer_id_lambda
+}
+
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - Study Path
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "post_study" {
+  api_id             = local.apigw_id
+  route_key          = "POST /study/{proxy+}"
+  target             = "integrations/${local.apigw_integration_id_backend}"
+  authorization_type = "CUSTOM"
+  authorizer_id      = local.apigw_authorizer_id_lambda
+}
