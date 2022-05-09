def0f00cb671f9901d901994aa5990971525d7a7
wwalpha <wwalpha@gmail.com>
2022-05-09 21:24:18 +0900

    remove files

--- frontend/react/users/src/containers/admin/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as AdminTop } from './AdminTop';
-export { default as DrawerLeft } from './DrawerLeft';
-export { default as Mainboard } from './Mainboard';
-export { default as Students } from './Students';
-export { default as Settings } from './Settings';
-export { default as GroupDetails } from './GroupDetails';
--- frontend/react/users/src/containers/questions/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as QuestionConfirm } from './UploadConfirm';
-export { default as QuestionList } from './QuestionList';
-export { default as QuestionMain } from './Main';
