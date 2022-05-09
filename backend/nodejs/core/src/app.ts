70df85f7b6f6bccd20b642fd7610b61258405a0c
wwalpha <wwalpha@gmail.com>
2022-05-09 11:20:25 +0900

    update

--- backend/nodejs/core/src/app.ts
+++ backend/nodejs/core/src/app.ts
@@ -54,39 +54,39 @@ app.put('/v1/groups/:groupId', express.json(), (req, res) => entry(req, res, Gro
 app.delete('/v1/groups/:groupId', express.json(), (req, res) => entry(req, res, GroupRemove));
 
 // グループ学習状態
-app.get('/v1/groups/:groupId/status', express.json(), (req, res) => entry(req, res, B006 as any));
-// 単語一括登録
-app.post('/v1/groups/:groupId/words', express.json(), (req, res) => entry(req, res, C001 as any));
-// 単語一括取得
-app.get('/v1/groups/:groupId/words', express.json(), (req, res) => entry(req, res, C002 as any));
-// 単語情報取得
-app.get('/v1/groups/:groupId/words/:word', express.json(), (req, res) => entry(req, res, C003 as any));
-// 単語情報更新
-app.put('/v1/groups/:groupId/words/:word', express.json(), (req, res) => entry(req, res, C004 as any));
-// 単語情報削除
-app.delete('/v1/groups/:groupId/words/:word', express.json(), (req, res) => entry(req, res, C005 as any));
-// 新規学習モード単語一覧
-app.get('/v1/groups/:groupId/new', express.json(), (req, res) => entry(req, res, C006 as any));
-// テストモード単語一覧
-app.get('/v1/groups/:groupId/test', express.json(), (req, res) => entry(req, res, C007 as any));
-// 復習モード単語一覧
-app.get('/v1/groups/:groupId/review', express.json(), (req, res) => entry(req, res, C008));
-// 画像から単語に変換する
-app.post('/v1/image2text', express.json(), (req, res) => entry(req, res, D001));
-// 画像から単語に変換する
-app.post('/v1/user/wordignore', express.json(), (req, res) => entry(req, res, D003));
+// app.get('/v1/groups/:groupId/status', express.json(), (req, res) => entry(req, res, B006 as any));
+// // 単語一括登録
+// app.post('/v1/groups/:groupId/words', express.json(), (req, res) => entry(req, res, C001 as any));
+// // 単語一括取得
+// app.get('/v1/groups/:groupId/words', express.json(), (req, res) => entry(req, res, C002 as any));
+// // 単語情報取得
+// app.get('/v1/groups/:groupId/words/:word', express.json(), (req, res) => entry(req, res, C003 as any));
+// // 単語情報更新
+// app.put('/v1/groups/:groupId/words/:word', express.json(), (req, res) => entry(req, res, C004 as any));
+// // 単語情報削除
+// app.delete('/v1/groups/:groupId/words/:word', express.json(), (req, res) => entry(req, res, C005 as any));
+// // 新規学習モード単語一覧
+// app.get('/v1/groups/:groupId/new', express.json(), (req, res) => entry(req, res, C006 as any));
+// // テストモード単語一覧
+// app.get('/v1/groups/:groupId/test', express.json(), (req, res) => entry(req, res, C007 as any));
+// // 復習モード単語一覧
+// app.get('/v1/groups/:groupId/review', express.json(), (req, res) => entry(req, res, C008));
+// // 画像から単語に変換する
+// app.post('/v1/image2text', express.json(), (req, res) => entry(req, res, D001));
+// // 画像から単語に変換する
+// app.post('/v1/user/wordignore', express.json(), (req, res) => entry(req, res, D003));
 
 // 今日のテスト単語一覧
-app.get('/v1/today/test', express.json(), (req, res) => entry(req, res, D004));
-// 今日の再学習単語一覧
-app.get('/v1/today/new', express.json(), (req, res) => entry(req, res, D005));
-// 今日の復習単語一覧
-app.get('/v1/today/review', express.json(), (req, res) => entry(req, res, D006));
+// app.get('/v1/today/test', express.json(), (req, res) => entry(req, res, D004));
+// // 今日の再学習単語一覧
+// app.get('/v1/today/new', express.json(), (req, res) => entry(req, res, D005));
+// // 今日の復習単語一覧
+// app.get('/v1/today/review', express.json(), (req, res) => entry(req, res, D006));
 
-// 単語詳細情報取得
-app.get('/v1/words/:word', express.json(), (req, res) => entry(req, res, E001 as any));
-// 単語詳細情報取得
-app.put('/v1/words/:word', express.json(), (req, res) => entry(req, res, E002 as any));
+// // 単語詳細情報取得
+// app.get('/v1/words/:word', express.json(), (req, res) => entry(req, res, E001 as any));
+// // 単語詳細情報取得
+// app.put('/v1/words/:word', express.json(), (req, res) => entry(req, res, E002 as any));
 
 // 問題一括登録
 app.post('/v1/groups/:groupId/questions', express.json(), (req, res) => entry(req, res, QuestionRegist));
