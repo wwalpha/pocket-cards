4610991a20da2370ebf67186f2933e64895dbe9b
wwalpha <wwalpha@gmail.com>
2022-05-18 15:07:32 +0900

    update

--- backend/terraform/apigw_route_curriculums.tf
+++ backend/terraform/apigw_route_curriculums.tf
@@ -1,9 +1,31 @@
 # ---------------------------------------------------------------------------------------------
 # API Gateway Route - Curriculums Proxy
 # ---------------------------------------------------------------------------------------------
-resource "aws_apigatewayv2_route" "get_curriculums_proxy" {
+resource "aws_apigatewayv2_route" "delete_curriculums_proxy" {
   api_id             = local.apigw_id
   route_key          = "DELETE /curriculums/{proxy+}"
+  target             = "integrations/${local.apigw_integration_id_backend}"
+  authorization_type = "CUSTOM"
+  authorizer_id      = local.apigw_authorizer_id_lambda
+}
+
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - Curriculums Proxy
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "post_curriculums_proxy" {
+  api_id             = local.apigw_id
+  route_key          = "POST /curriculums/{proxy+}"
+  target             = "integrations/${local.apigw_integration_id_backend}"
+  authorization_type = "CUSTOM"
+  authorizer_id      = local.apigw_authorizer_id_lambda
+}
+
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - Curriculums Proxy
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "get2_curriculums_proxy" {
+  api_id             = local.apigw_id
+  route_key          = "GET /curriculums/{proxy+}"
   target             = "integrations/${local.apigw_integration_id_backend}"
   authorization_type = "CUSTOM"
   authorizer_id      = local.apigw_authorizer_id_lambda
