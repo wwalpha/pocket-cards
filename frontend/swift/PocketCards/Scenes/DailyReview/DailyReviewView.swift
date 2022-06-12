e1673271a69f173c6bd0ff3e1928ba51ccb10438
wwalpha <wwalpha@gmail.com>
2022-06-12 18:52:11 +0900

    save

--- frontend/swift/PocketCards/Scenes/DailyReview/DailyReviewView.swift
+++ frontend/swift/PocketCards/Scenes/DailyReview/DailyReviewView.swift
@@ -3,11 +3,10 @@
 //  PocketCards
 //
 //  Created by macmini on 2022/06/12.
-//  
+//
 //
 
 import SwiftUI
-
 
 struct DailyReviewView: View {
     var interactor: DailyReviewBusinessLogic?
@@ -15,18 +14,47 @@ struct DailyReviewView: View {
     @ObservedObject var viewModel = DailyReviewViewModel()
 
     var body: some View {
-        Text("Hello")
+        if viewModel.isLoading {
+            Text("Loading....")
+                .onAppear {
+                    Task {
+                        await interactor?.loadQuestions()
+                    }
+                }
+        } else if viewModel.isFinish {
+            Text("今日の学習は終わりました")
+                .font(.system(size: 64, design: .default))
+        } else {
+            if viewModel.question?.choices != nil {
+                ChoiceQuestion(
+                    question: viewModel.question!,
+                    isShowError: "",
+                    onChoice: interactor!.onChoice
+                )
+            } else {
+                // Society or Science
+                FlashCard(question: viewModel.question!, action: interactor!.onAction)
+            }
+        }
     }
 }
 
 extension DailyReviewView: DailyReviewDisplayLogic {
+    func showNext(model: DailyStudyViewModel) {
+        DispatchQueue.main.async {
+            viewModel.isLoading = model.isLoading
+            viewModel.isFinish = model.isFinish
+            viewModel.isShowError = model.isShowError
+        }
 
+        viewModel.question = model.question
+    }
 }
 
 extension DailyReviewView {
-    func configureView() -> some View {
+    func configureView(subject: String) -> some View {
         var view = self
-        let interactor = DailyReviewInteractor()
+        let interactor = DailyReviewInteractor(subject: subject)
         let presenter = DailyReviewPresenter()
 
         view.interactor = interactor
