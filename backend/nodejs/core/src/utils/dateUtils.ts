0cb2ee98b3222bb0e851a5820c87e44b044474c6
wwalpha <wwalpha@gmail.com>
2022-06-27 15:04:20 +0900

    update

--- backend/nodejs/core/src/utils/dateUtils.ts
+++ backend/nodejs/core/src/utils/dateUtils.ts
@@ -1,19 +1,29 @@
+import { Consts } from '@consts';
 import moment from 'moment';
 
 export const getNow = () => `${moment().format('YYYYMMDD')}`;
 
+// 算数以外の科目の日数
 const days = [1, 2, 4, 7, 15, 30, 60, 90, 180, 360];
+// 算数の日数
+const daysM = [1, 3, 7, 15, 60, 120];
 
 /** 次回学習時間を計算する */
-export const getNextTime = (times: number) => {
+export const getNextTime = (times: number, subject?: string) => {
   if (times === 0) return getNow();
-  // 10 回学習後表示しないようにする
-  if (times > 10) return '99991231';
+  // 算数以外の場合、10 回学習後表示しないようにする
+  if (times > 10 && subject !== Consts.SUBJECT.MATHS) return '99991231';
+  // 7 回学習後表示しないようにする
+  if (times > 7 && subject === Consts.SUBJECT.MATHS) return '99991231';
 
-  const addValue = days[times - 1];
-
+  // 日数の組みの選択
+  const nextDays = Consts.SUBJECT.MATHS === subject ? daysM : days;
+  // 間隔の日数
+  const addValue = nextDays[times - 1];
+  // 次の日付
   const nextTime = moment().add(addValue, 'days').format('YYYYMMDD');
 
+  // 文字列を返す
   return `${nextTime}`;
 };
 
