967d8adf86c2739c0545b96bb751543431440dbc
wwalpha <wwalpha@gmail.com>
2022-08-29 19:22:35 +0900

    update

--- backend/nodejs/core/src/apis/questions/ignore.ts
+++ backend/nodejs/core/src/apis/questions/ignore.ts
@@ -1,8 +1,8 @@
 import { Request } from 'express';
 import { Consts } from '@consts';
 import { APIs } from 'typings';
-import { GroupService, QuestionService, WordService } from '@services';
-import { ValidationError } from '@utils';
+import { CurriculumService, GroupService, LearningService, QuestionService, WordService } from '@services';
+import { Commons, ValidationError } from '@utils';
 
 /** 今日のテスト */
 export default async (
@@ -10,54 +10,56 @@ export default async (
 ): Promise<APIs.QuestionIgnoreResponse> => {
   const { groupId } = req.params;
   const { qid } = req.body;
+  const userId = Commons.getUserId(req);
 
-  const groupInfo = await GroupService.describe(groupId);
+  // get all informations
+  const results = await Promise.all([
+    GroupService.describe(groupId),
+    QuestionService.describe(qid),
+    LearningService.describe(qid, userId),
+    CurriculumService.queryByGroup(groupId, userId),
+  ]);
+
+  const [groupInfo, question, learning, curriculum] = results;
 
   // 英語以外の場合、無効です
   if (groupInfo?.subject !== Consts.SUBJECT.ENGLISH) {
-    throw new ValidationError('Only effect to english subject.');
+    throw new ValidationError('Only effect to English.');
   }
-
-  const question = await QuestionService.describe(qid);
-
   // question not found
   if (!question) {
     throw new ValidationError('Question not found.');
   }
 
-  // 学習詳細取得する
-  // const results = await Promise.all([LearningService.listByQuestion(questionId), QuestionService.describe(questionId)]);
+  // learning status not found
+  if (!learning) {
+    throw new ValidationError('Learning infomartion not found.');
+  }
 
-  // const learning = results[0];
-  // const question = results[1];
+  // Curriculum not found
+  if (!curriculum) {
+    throw new ValidationError('Curriculum not found.');
+  }
 
-  // const tasks = learning.map(async (item) => {
-  //   // 未学習の場合、カウントを引く
-  //   if (item.lastTime === Consts.INITIAL_DATE) {
-  //     // 対象カリキュラムを検索する
-  //     const curriculums = await CurriculumService.listByGroup(item.groupId, item.userId);
-
-  //     // 処理実行
-  //     await Promise.all(
-  //       curriculums.map(async (c) => {
-  //         await CurriculumService.updateUnlearned(c.id, -1);
-  //       })
-  //     );
-  //   }
-
-  //   // 学習対象から削除する
-  //   await LearningService.remove(item.qid, item.userId);
-  // });
-
-  // // 学習実績を削除する
-  // await Promise.all(tasks);
-
-  // 単語削除、無視単語の追加
-  await Promise.all([
+  const tasks = [
+    // グループの問題数を調整する
+    GroupService.minusCount(groupId, 1),
+    // 問題を削除する
     QuestionService.remove(question.id),
+    // 学習状況を削除する
+    LearningService.remove(qid, userId),
+    // 単語無視に登録する
     WordService.registIgnore({
       id: Consts.Authority.ADMIN,
       word: question.title,
     }),
-  ]);
+  ];
+
+  // 未学習の場合
+  if (learning.lastTime === Consts.INITIAL_DATE) {
+    tasks.push(CurriculumService.updateUnlearned(curriculum.id, -1));
+  }
+
+  // 単語削除、無視単語の追加
+  await Promise.all(tasks);
 };
