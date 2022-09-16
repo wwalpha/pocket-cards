39d0c97f4bdb19364921ae2c20537b20c65106b8
wwalpha <wwalpha@gmail.com>
2022-09-16 11:13:54 +0900

    update

--- backend/nodejs/lambda/wss_commands/src/index.ts
+++ backend/nodejs/lambda/wss_commands/src/index.ts
@@ -1,6 +1,21 @@
-export const handler = async (event: any): Promise<any> => {
+import { APIGatewayProxyWebsocketEventV2 } from 'aws-lambda';
+import { ApiGatewayManagementApi } from 'aws-sdk';
+
+export const handler = async (event: APIGatewayProxyWebsocketEventV2, context: any): Promise<any> => {
+  console.log(context);
+  const { connectionId, domainName, stage } = event.requestContext;
+
+  const apigateway = new ApiGatewayManagementApi({ endpoint: `${domainName}/${stage}` });
+
+  console.log(event.requestContext);
+  // await apigateway
+  //   .postToConnection({
+  //     ConnectionId: connectionId as string,
+  //     Data: 'Hi Client',
+  //   })
+  //   .promise();
+
   return {
     statusCode: 200,
-    isBase64Encoded: false,
   };
 };
