f54babbf5efd75cd68ddc03db349ab14154d22fc
wwalpha <wwalpha@gmail.com>
2022-09-18 23:26:36 +0900

    update

--- backend/nodejs/lambda/authorizer_v1/src/index.ts
+++ backend/nodejs/lambda/authorizer_v1/src/index.ts
@@ -1,16 +1,23 @@
+import { DynamoDB } from 'aws-sdk';
 import { APIGatewayAuthorizerResult, APIGatewayRequestAuthorizerEvent } from 'aws-lambda';
 import { JwtPayload } from 'jsonwebtoken';
 import winston from 'winston';
 import omit from 'lodash/omit';
 import { decodeToken, getPublicKeys, validateToken } from './utils';
 import { ApiOptions, AuthPolicy } from './AuthPolicy';
+import { Tables } from 'typings';
 
+const TABLE_NAME_USERS = process.env.TABLE_NAME_USERS as string;
 const PEM_KEYS: Record<string, Record<string, string>> = {};
 const API_KEYS: string[] = [];
 const Logger = winston.createLogger({
   level: 'info',
   defaultMeta: { service: 'authorizer' },
   transports: [new winston.transports.Console()],
+});
+
+const client = new DynamoDB.DocumentClient({
+  region: process.env.AWS_DEFAULT_REGION,
 });
 
 export const handler = async (event: APIGatewayRequestAuthorizerEvent): Promise<APIGatewayAuthorizerResult> => {
@@ -54,6 +61,12 @@ export const handler = async (event: APIGatewayRequestAuthorizerEvent): Promise<
     const principalId = payload['cognito:username'];
     // policy
     const policy = buildAuthPolicy(event, principalId);
+
+    const guadian = await getGuardian(principalId);
+
+    policy.context = {
+      guadian: guadian,
+    };
 
     console.log(JSON.stringify(policy));
 
@@ -101,4 +114,23 @@ const authorizationFailure = (): APIGatewayAuthorizerResult => {
   policy.denyAllMethods();
 
   return policy.build();
+};
+
+const getGuardian = async (userId: string): Promise<string | undefined> => {
+  // get user role from db
+  const result = await client
+    .get({
+      TableName: TABLE_NAME_USERS,
+      Key: {
+        id: userId,
+      } as Tables.TUsersKey,
+    })
+    .promise();
+
+  // user not found
+  if (!result.Item) return undefined;
+
+  const userInfo = result.Item as Tables.TUsers;
+
+  return userInfo.teacher ? userInfo.teacher : userInfo.id;
 };
