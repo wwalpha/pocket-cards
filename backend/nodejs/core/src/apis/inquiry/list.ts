5467811c8dacf744954ebd39fb4c31a163eacf81
wwalpha <wwalpha@gmail.com>
2022-10-05 09:06:19 +0900

    bugfix

--- backend/nodejs/core/src/apis/inquiry/list.ts
+++ backend/nodejs/core/src/apis/inquiry/list.ts
@@ -1,12 +1,15 @@
 import { Request } from 'express';
 import { APIs } from 'typings';
-import { InquiryService } from '@services';
+import { InquiryService, QuestionService } from '@services';
 
 export default async (_: Request<any, any, APIs.InquiryListResquest, any>): Promise<APIs.InquiryListResponse> => {
   // データ登録
   const items = await InquiryService.getAll();
 
+  // 問題一覧
+  const questions = await Promise.all(items.map((item) => QuestionService.describe(item.qid)));
+
   return {
-    items,
+    items: questions.filter((item): item is Exclude<typeof item, undefined> => item !== undefined),
   };
 };
