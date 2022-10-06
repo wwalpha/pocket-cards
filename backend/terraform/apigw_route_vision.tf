d11a1427871b09f91858f2844c4f472d5bf3f378
wwalpha <wwalpha@gmail.com>
2022-10-06 13:30:35 +0900

    #14 bugfix

--- backend/terraform/apigw_route_vision.tf
+++ backend/terraform/apigw_route_vision.tf
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------------------------------------------
 resource "aws_apigatewayv2_route" "post_vision" {
   api_id             = local.apigw_id
-  route_key          = "POST /vision/*"
+  route_key          = "POST /vision/{proxy+}"
   target             = "integrations/${local.apigw_integration_id_backend}"
   authorization_type = "CUSTOM"
   authorizer_id      = local.apigw_authorizer_id_lambda
