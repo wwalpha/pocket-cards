a14b55564406b5b2776607df8ad871c52b69c129
wwalpha <wwalpha@gmail.com>
2022-10-06 13:06:30 +0900

    deleted

--- backend/nodejs/core/src/apis/d0/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as D001 } from './d001';
-export { default as D003 } from './d003';
-export { default as D004 } from './d004';
-export { default as D005 } from './d005';
-export { default as D006 } from './d006';
