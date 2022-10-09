7fda2b8effddd25a41335623beac188e763a7a3c
wwalpha <wwalpha@gmail.com>
2022-10-09 14:55:57 +0900

    #16 add test case

--- backend/nodejs/core/test/datas/weekly/index.ts
+++ backend/nodejs/core/test/datas/weekly/index.ts
@@ -2,3 +2,6 @@ export { default as REGIST_01_EXPECT } from './regist/001_expect.json';
 
 export { default as LIST_01_DB_LEARNING } from './list/001_db_learning.json';
 export { default as LIST_01_EXPECT } from './list/001_expect.json';
+
+export { default as ANSWER_01_EXPECT } from './answer/001_expect.json';
+export { default as ANSWER_02_EXPECT } from './answer/002_expect.json';
