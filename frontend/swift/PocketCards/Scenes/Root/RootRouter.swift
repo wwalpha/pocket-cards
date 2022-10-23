d7aa12fba791b654fb06d73d07c5f7924500b5a5
wwalpha <wwalpha@gmail.com>
2022-10-23 22:43:43 +0900

    bugfix

--- frontend/swift/PocketCards/Scenes/Root/RootRouter.swift
+++ frontend/swift/PocketCards/Scenes/Root/RootRouter.swift
@@ -29,7 +29,7 @@ class RootRouter {
     }
 
     func makeScienceStudyiew() -> some View {
-        DailyStudyView().configureView(subject: SUBJECT.SCIENCE, loadUrl: URLs.STUDY_DAILY_CURRICULUM_ORDER)
+        DailyStudyView().configureView(subject: SUBJECT.SCIENCE, loadUrl: URLs.STUDY_DAILY_PRACTICE)
     }
 
     func makeScienceTestView() -> some View {
@@ -37,7 +37,7 @@ class RootRouter {
     }
 
     func makeSocietyStudyiew() -> some View {
-        DailyStudyView().configureView(subject: SUBJECT.SOCIETY, loadUrl: URLs.STUDY_DAILY_CURRICULUM_ORDER)
+        DailyStudyView().configureView(subject: SUBJECT.SOCIETY, loadUrl: URLs.STUDY_DAILY_PRACTICE)
     }
 
     func makeSocietyTestView() -> some View {
