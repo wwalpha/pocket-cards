917f155ecb9ef003252ff4573cd7adfb721bbb94
wwalpha <wwalpha@gmail.com>
2022-10-23 10:38:05 +0900

    #19

--- frontend/react/admin/src/reducers/study.ts
+++ frontend/react/admin/src/reducers/study.ts
@@ -5,6 +5,8 @@ import * as StudyActions from './studyActions';
 
 const studyState: Domains.StudyState = {
   isOnline: false,
+  correctCount: 0,
+  incorrectCount: 0,
   answered: [],
   questions: [],
   index: -1,
@@ -31,16 +33,22 @@ const slice = createSlice({
         state.isOnline = false;
       }
     },
-    STUDY_SHOW_ANSWER: (state, { payload }: PayloadAction<string>) => {
+    STUDY_SHOW_ANSWER: (state) => {
       const question = state.questions[state.index];
       // answerd question id
       state.answered.push(question.id);
+      // count incorrect
+      state.incorrectCount += 1;
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(StudyActions.STUDY_QUESTIONS.fulfilled, (state, { payload }) => {
+        // initialize
         state.questions = payload;
+        state.answered = [];
+        state.correctCount = 0;
+        state.incorrectCount = 0;
         state.index = 0;
       })
       .addCase(StudyActions.STUDY_SHOW_QUESTION.fulfilled, (state, { payload }) => {
@@ -50,6 +58,8 @@ const slice = createSlice({
         }
         // remove question
         state.questions = state.questions.filter((item) => item.id !== state.questions[state.index].id);
+        // count correct
+        state.correctCount += 1;
         // recalculate index
         if (state.index + 1 >= state.questions.length) {
           state.index = 0;
