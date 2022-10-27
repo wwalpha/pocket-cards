107da5e198f544ba509a98311adaf725903f352b
wwalpha <wwalpha@gmail.com>
2022-10-27 12:06:51 +0900

    【Backend】カリキュラム別に、学習進捗の検索 #40

--- backend/nodejs/core/src/apis/reports/index.ts
+++ backend/nodejs/core/src/apis/reports/index.ts
@@ -3,5 +3,5 @@ export { default as LearningProgress } from './progress';
 export { default as LearningOverall } from './overall';
 export { default as DailyStatus } from './dailyStatus';
 export { default as OverallStatus } from './overallStatus';
-export { default as GroupStatus } from './groupStatus';
+export { default as CurriculumStatus } from './curriculumStatus';
 export { default as DailyTestQuestions } from './questions/dailyTest';
