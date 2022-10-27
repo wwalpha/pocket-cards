3628ea08f311942d70accade6e5398998cbdd564
wwalpha <wwalpha@gmail.com>
2022-10-27 11:17:54 +0900

    search form

--- frontend/react/admin/src/actions/progress.ts
+++ frontend/react/admin/src/actions/progress.ts
@@ -1,20 +1,13 @@
 import { Actions } from '@reducers';
 import { AppDispatch } from 'typings';
 
-export const search = (student: string, subject: string) => async (dispatch: AppDispatch) => {
-  // 検索条件を保存する
-  dispatch(
-    Actions.PROGRESS_SAVE_CONDITIONS({
-      student: student,
-      subject: subject,
-    })
-  );
-
+export const search = (student: string, subject: string, curriculum: string) => async (dispatch: AppDispatch) => {
   // 検索する
   dispatch(
     Actions.PROGRESS_SEARCH({
       student: student,
       subject: subject,
+      curriculum: curriculum,
     })
   );
 };
