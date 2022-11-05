d3dcc49601975ca8da450ca1afc9c0a6304f29e8
wwalpha <wwalpha@gmail.com>
2022-11-05 19:44:51 +0900

    rename function

--- backend/nodejs/auth_manager/src/server.ts
+++ backend/nodejs/auth_manager/src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import { json, urlencoded } from 'body-parser';
-import { login, common, healthCheck, initiateAuth } from './app';
+import { login, common, healthCheck, refreshToken } from './app';
 
 // Instantiate application
 const app = express();
@@ -18,12 +18,6 @@ app.get('/v1/auth/health', async (req, res) => await common(req, res, healthChec
 app.post('/v1/auth/login', async (req, res) => await common(req, res, login));
 
 // refresh tokens
-app.post('/v1/auth/refresh', async (req, res) => await common(req, res, initiateAuth));
-
-// get system version no
-// app.get('/system/version', async (req, res) => await common(req, res, version));
-
-// // get release information
-// app.get('/system/releases', async (req, res) => await common(req, res, release));
+app.post('/v1/auth/refresh', async (req, res) => await common(req, res, refreshToken));
 
 export default app;
