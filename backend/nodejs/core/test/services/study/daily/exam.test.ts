ef78a76a8ec0c72b61b45d7c067ea400bc4e759b
wwalpha <wwalpha@gmail.com>
2022-11-06 09:27:40 +0900

    【API】自己テスト問題取得のURL変更 #93

--- backend/nodejs/core/test/services/study/daily/exam.test.ts
+++ backend/nodejs/core/test/services/study/daily/exam.test.ts
@@ -1,7 +1,7 @@
 import server from '@src/app';
 import request from 'supertest';
-import * as COMMONS from '../../datas/commons';
-import * as QUESTIONS from '../../datas/questions/exam';
+import * as COMMONS from '../../../datas/commons';
+import * as QUESTIONS from '../../../datas/questions/exam';
 import { HEADER_GUARDIAN, HEADER_USER } from '@test/Commons';
 import { DynamodbHelper } from '@alphax/dynamodb';
 import { Environment } from '@consts';
@@ -11,7 +11,7 @@ const client = new DynamodbHelper({ options: { endpoint: process.env['AWS_ENDPOI
 jest.mock('axios');
 jest.setTimeout(10000);
 
-describe('Questions', () => {
+describe('Study', () => {
   beforeEach(async () => {
     await Promise.all([
       client.bulk(Environment.TABLE_NAME_GROUPS, COMMONS.DB_GROUPS),
@@ -31,7 +31,7 @@ describe('Questions', () => {
   });
 
   test('Exam01: デリー学習一覧_未学習のみ', async () => {
-    const apiPath = `/v1/study/daily/test/questions`;
+    const apiPath = `/v1/study/daily/exam`;
 
     const res = await request(server)
       .get(apiPath)
