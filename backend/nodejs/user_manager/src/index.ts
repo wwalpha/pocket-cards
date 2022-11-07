f5af9b7585aefc338dfd25b5f1bd327d3fab6214
wwalpha <wwalpha@gmail.com>
2022-11-07 19:34:53 +0900

    【Express】timeout 時間設定する #117

--- backend/nodejs/user_manager/src/index.ts
+++ backend/nodejs/user_manager/src/index.ts
@@ -3,6 +3,9 @@ import app from './server';
 const PORT = process.env.PORT || 8080;
 
 // Start the servers
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`User manager service started on port ${PORT}`);
 });
+
+// timeout 20s
+server.timeout = 1000 * 20;
