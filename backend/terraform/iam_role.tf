aa0709c5302ba51031521934bd18689d8c002103
wwalpha <wwalpha@gmail.com>
2023-02-13 18:21:23 +0900

    update

--- backend/terraform/iam_role.tf
+++ backend/terraform/iam_role.tf
@@ -115,6 +115,14 @@ resource "aws_iam_role_policy_attachment" "batch_ses" {
 }
 
 # ----------------------------------------------------------------------------------------------
+# AWS Lambda Role - Batch Athena Policy
+# ----------------------------------------------------------------------------------------------
+resource "aws_iam_role_policy_attachment" "batch_athena" {
+  role       = aws_iam_role.batch.name
+  policy_arn = aws_iam_policy.athena_basic.arn
+}
+
+# ----------------------------------------------------------------------------------------------
 # AWS Lambda Role - Vision
 # ----------------------------------------------------------------------------------------------
 resource "aws_iam_role" "vision" {
--- backend/terraform/lambda.tf
+++ backend/terraform/lambda.tf
@@ -107,16 +107,16 @@ resource "aws_lambda_function" "batch" {
 
   environment {
     variables = {
-      TABLE_NAME_USERS       = local.dynamodb_name_users
-      TABLE_NAME_TRACES      = local.dynamodb_name_traces
-      TABLE_NAME_LEARNING    = local.dynamodb_name_learning
-      TABLE_NAME_REPORTS     = local.dynamodb_name_reports
-      TABLE_NAME_GROUP       = local.dynamodb_name_groups
-      TABLE_NAME_ACCURACY    = local.dynamodb_name_accuracy
-      ATHENAM_SCHEMA_NAME    = local.athena_schema_name
-      ATHENAM_WORKGROUP_NAME = local.athena_workgroup_name
-      MASTER_EMAIL_ADDRESS   = "master@${local.domain_name}"
-      TZ                     = "Asia/Tokyo"
+      TABLE_NAME_USERS      = local.dynamodb_name_users
+      TABLE_NAME_TRACES     = local.dynamodb_name_traces
+      TABLE_NAME_LEARNING   = local.dynamodb_name_learning
+      TABLE_NAME_REPORTS    = local.dynamodb_name_reports
+      TABLE_NAME_GROUP      = local.dynamodb_name_groups
+      TABLE_NAME_ACCURACY   = local.dynamodb_name_accuracy
+      ATHENA_SCHEMA_NAME    = local.athena_schema_name
+      ATHENA_WORKGROUP_NAME = local.athena_workgroup_name
+      MASTER_EMAIL_ADDRESS  = "master@${local.domain_name}"
+      TZ                    = "Asia/Tokyo"
     }
   }
 }
