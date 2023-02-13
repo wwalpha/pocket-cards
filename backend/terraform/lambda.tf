c1a9df71b4bc17863cad54d0fc9ae17057ccedee
wwalpha <wwalpha@gmail.com>
2023-02-13 11:57:27 +0900

    add environment variable

--- backend/terraform/lambda.tf
+++ backend/terraform/lambda.tf
@@ -111,6 +111,7 @@ resource "aws_lambda_function" "batch" {
       TABLE_NAME_TRACES    = local.dynamodb_name_traces
       TABLE_NAME_LEARNING  = local.dynamodb_name_learning
       TABLE_NAME_REPORTS   = local.dynamodb_name_reports
+      TABLE_NAME_ACCURACY  = local.dynamodb_name_accuracy
       MASTER_EMAIL_ADDRESS = "master@${local.domain_name}"
       TZ                   = "Asia/Tokyo"
     }
