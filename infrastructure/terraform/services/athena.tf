6d260694b0d700dfd783e7a4da3e96d7fc74c920
wwalpha <wwalpha@gmail.com>
2023-02-13 11:31:39 +0900

    add athena settings

---
+++ infrastructure/terraform/services/athena.tf
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------------------------------------------------
+# AWS Athena Workgroup
+# -----------------------------------------------------------------------------------------------------
+resource "aws_athena_workgroup" "this" {
+  name = "${local.project_name}-workspace"
+
+  configuration {
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = false
+
+    result_configuration {
+      output_location = "s3://${local.bucket_name_archive}/athena/"
+    }
+  }
+}
+
+# -----------------------------------------------------------------------------------------------------
+# AWS Athena Database
+# -----------------------------------------------------------------------------------------------------
+resource "aws_athena_database" "this" {
+  name   = "${local.project_name}_database"
+  bucket = local.bucket_name_archive
+}
