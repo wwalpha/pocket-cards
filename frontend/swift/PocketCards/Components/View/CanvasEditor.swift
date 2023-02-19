203fb63fd68b471757f4cf51e2ec26d308502797
wwalpha <wwalpha@gmail.com>
2023-02-19 22:27:07 +0900

    change path

---
+++ frontend/swift/PocketCards/Components/View/CanvasEditor.swift
@@ -0,0 +1,20 @@
+//
+//  CanvasEditor.swift
+//  PocketCards
+//
+//  Created by macmini on 2023/02/19.
+//
+
+import SwiftUI
+
+struct CanvasEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CanvasEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        CanvasEditor()
+    }
+}
