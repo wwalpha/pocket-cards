24badf2837bc3bbf6a43c1bf190c7fdd479a1737
wwalpha <wwalpha@gmail.com>
2023-02-20 10:55:03 +0900

    【Backend】進捗確認の絞り条件追加 #188

--- frontend/react/admin/src/actions/progress.ts
+++ frontend/react/admin/src/actions/progress.ts
@@ -1,11 +1,12 @@
 import { Actions } from '@reducers';
 import { AppDispatch } from 'typings';
 
-export const search = (curriculums: string[]) => async (dispatch: AppDispatch) => {
+export const search = (curriculums: string[], period?: string) => async (dispatch: AppDispatch) => {
   // 検索する
   dispatch(
     Actions.PROGRESS_SEARCH({
       curriculums: curriculums,
+      period: period,
     })
   );
 };
--- frontend/react/admin/src/reducers/progressActions.ts
+++ frontend/react/admin/src/reducers/progressActions.ts
@@ -4,16 +4,14 @@ import { API } from '@utils';
 import { APIs } from 'typings';
 
 // 進捗検索
-export const PROGRESS_SEARCH = createAsyncThunk<APIs.CurriculumStatusResponseItem[], { curriculums: string[] }>(
-  'progress/PROGRESS_SEARCH',
-  async ({ curriculums }) => {
-    const res = await API.post<APIs.CurriculumStatusResponse, APIs.CurriculumStatusRequest>(
-      URLs.CURRICULUM_PROGRESS(),
-      {
-        curriculums: curriculums,
-      }
-    );
+export const PROGRESS_SEARCH = createAsyncThunk<
+  APIs.CurriculumStatusResponseItem[],
+  { curriculums: string[]; period?: string }
+>('progress/PROGRESS_SEARCH', async ({ curriculums, period }) => {
+  const res = await API.post<APIs.CurriculumStatusResponse, APIs.CurriculumStatusRequest>(URLs.CURRICULUM_PROGRESS(), {
+    curriculums: curriculums,
+    endDate: period,
+  });
 
-    return res.items;
-  }
-);
+  return res.items;
+});
