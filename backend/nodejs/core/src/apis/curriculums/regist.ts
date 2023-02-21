1e07cd61b1ecffd477e515b3edffdbd0d0d704f6
wwalpha <wwalpha@gmail.com>
2023-02-21 12:01:29 +0900

    add minus one times

--- backend/nodejs/core/src/apis/curriculums/regist.ts
+++ backend/nodejs/core/src/apis/curriculums/regist.ts
@@ -41,7 +41,7 @@ export default async (
     subject: groupInfo.subject,
     lastTime: Consts.INITIAL_DATE,
     nextTime: Consts.INITIAL_DATE,
-    times: 0,
+    times: -1,
   }));
 
   // bulk insert
