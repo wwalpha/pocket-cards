a1c693e076052d1cd0798bddda689e8a3f2f2dac
wwalpha <wwalpha@gmail.com>
2023-02-22 15:11:42 +0900

    update aws-sdk v2 to v3

--- backend/nodejs/core/src/utils/clientUtils.ts
+++ backend/nodejs/core/src/utils/clientUtils.ts
@@ -1,10 +1,12 @@
-import * as AWSSDK from 'aws-sdk';
-import { DynamoDB, Polly, S3, Translate, SSM, Lambda, TimestreamWrite } from 'aws-sdk';
+import { S3, S3ClientConfig } from '@aws-sdk/client-s3';
+import { DynamoDB, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
+import { Polly, PollyClientConfig } from '@aws-sdk/client-polly';
+import { Translate, TranslateClientConfig } from '@aws-sdk/client-translate';
+import { SSM, SSMClientConfig } from '@aws-sdk/client-ssm';
+import { Lambda, LambdaClientConfig } from '@aws-sdk/client-lambda';
+import { TimestreamWrite, TimestreamWriteClientConfig } from '@aws-sdk/client-timestream-write';
 
-// export const AWS = process.env.ENVIRONMENT === 'local' ? AWSSDK : XRay.captureAWS(AWSSDK);
-const AWS = AWSSDK;
-
-let dynamoDBClient: DynamoDB.DocumentClient;
+let dynamoDBClient: DynamoDB;
 let pollyClient: Polly;
 let s3Client: S3;
 let translateClient: Translate;
@@ -13,36 +15,34 @@ let lambdaClient: Lambda;
 let writeClient: TimestreamWrite;
 
 /** Dynamodb Client初期化 */
-export const dynamoDB = (
-  options?: DynamoDB.DocumentClient.DocumentClientOptions & DynamoDB.Types.ClientConfiguration
-): DynamoDB.DocumentClient => {
+export const dynamoDB = (options?: DynamoDBClientConfig): DynamoDB => {
   // 初期化済
   if (dynamoDBClient) return dynamoDBClient;
 
   // 初期化パラメータあり
-  if (options) return new AWS.DynamoDB.DocumentClient(options);
+  if (options) return new DynamoDB(options);
 
   // 初期化する
-  return new AWS.DynamoDB.DocumentClient({
+  return new DynamoDB({
     region: process.env['DEFAULT_REGION'],
   });
 };
 
 /** Polly Client初期化 */
-export const polly = (options?: Polly.ClientConfiguration): Polly => {
+export const polly = (options?: PollyClientConfig): Polly => {
   // 初期化済み
   if (pollyClient) return pollyClient;
 
   // 初期化設定あり
-  if (options) return new AWS.Polly(options);
+  if (options) return new Polly(options);
 
-  return new AWS.Polly({
+  return new Polly({
     region: process.env['DEFAULT_REGION'],
   });
 };
 
 /** S3 Client初期化 */
-export const s3 = (options?: S3.ClientConfiguration): S3 => {
+export const s3 = (options?: S3ClientConfig): S3 => {
   // 初期化済み
   if (s3Client) return s3Client;
 
@@ -50,62 +50,62 @@ export const s3 = (options?: S3.ClientConfiguration): S3 => {
   if (options) return new S3(options);
 
   // 初期化設定なし
-  return new AWS.S3({
+  return new S3({
     region: process.env['DEFAULT_REGION'],
     endpoint: process.env['AWS_ENDPOINT'],
   });
 };
 
 /** Translate初期化 */
-export const translate = (options?: Translate.ClientConfiguration): Translate => {
+export const translate = (options?: TranslateClientConfig): Translate => {
   // 初期化済み
   if (translateClient) return translateClient;
 
   // 初期化設定あり
-  if (options) return new AWS.Translate(options);
+  if (options) return new Translate(options);
 
   // 初期化設定なし
-  return new AWS.Translate({
+  return new Translate({
     region: process.env['DEFAULT_REGION'],
   });
 };
 
 /** SSM Client初期化 */
-export const ssm = (options?: SSM.ClientConfiguration): SSM => {
+export const ssm = (options?: SSMClientConfig): SSM => {
   // 初期化済み
   if (ssmClient) return ssmClient;
 
   // 初期化設定あり
-  if (options) return new AWS.SSM(options);
+  if (options) return new SSM(options);
 
   // 初期化設定なし
-  return new AWS.SSM({
+  return new SSM({
     region: process.env['DEFAULT_REGION'],
   });
 };
 
-export const lambda = (options?: Lambda.ClientConfiguration): Lambda => {
+export const lambda = (options?: LambdaClientConfig): Lambda => {
   // 初期化済み
   if (lambdaClient) return lambdaClient;
 
   // 初期化設定あり
-  if (options) return new AWS.Lambda(options);
+  if (options) return new Lambda(options);
 
   // 初期化設定なし
-  return new AWS.Lambda({
+  return new Lambda({
     region: process.env['DEFAULT_REGION'],
   });
 };
 
-export const timestreamWrite = (options?: Lambda.ClientConfiguration): TimestreamWrite => {
+export const timestreamWrite = (options?: TimestreamWriteClientConfig): TimestreamWrite => {
   // 初期化済み
   if (writeClient) return writeClient;
 
   // 初期化設定あり
-  if (options) return new AWS.TimestreamWrite(options);
+  if (options) return new TimestreamWrite(options);
 
   // 初期化設定なし
-  return new AWS.TimestreamWrite({
+  return new TimestreamWrite({
     region: process.env['DEFAULT_REGION'],
   });
 };
