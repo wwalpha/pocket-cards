7c97094f1e051b2849d40a1ec60548b0c8566dc3
wwalpha <wwalpha@gmail.com>
2023-02-27 19:57:08 +0900

    update

--- backend/nodejs/batch/src/func/index.ts
+++ backend/nodejs/batch/src/func/index.ts
@@ -1,10 +1,4 @@
-import AWS from 'aws-sdk';
 import { Environments } from '@utils';
-
-// update aws config
-AWS.config.update({
-  region: Environments.AWS_REGION,
-});
 
 export { default as CreateReports } from './reports';
 export { default as Scheduler } from './scheduler';
