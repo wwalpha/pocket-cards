c48ce69905d2fcc45c6b163e204086a744503652
wwalpha <wwalpha@gmail.com>
2023-03-12 17:58:43 +0900

    add new patch

---
+++ backend/nodejs/core/src/apis/patch/20230312/patch.ts
@@ -0,0 +1,18 @@
+import { LearningService } from '@services';
+
+export default async (): Promise<void> => {
+  const userId = 'Google_109439805128280065775';
+
+  const learnings = await LearningService.listByUser(userId);
+
+  const targets = learnings.filter((item) => item.subject === '1').filter((item) => item.times === 0);
+
+  await Promise.all(
+    targets.map((item) =>
+      LearningService.update({
+        ...item,
+        times: 1,
+      })
+    )
+  );
+};
