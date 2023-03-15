684043535a4a5212c2cebd8a46f9c62273bcd21d
wwalpha <wwalpha@gmail.com>
2023-03-15 14:54:47 +0900

    update

--- backend/nodejs/core/src/apis/patch/20230312/patch.ts
+++ backend/nodejs/core/src/apis/patch/20230312/patch.ts
@@ -6,7 +6,10 @@ const patch = async (): Promise<void> => {
 
   const learnings = await LearningService.listByUser(userId);
 
-  const targets = learnings.filter((item) => item.subject === '1').filter((item) => item.times === 0);
+  const targets = learnings
+    .filter((item) => item.subject === '1')
+    .filter((item) => item.times === 1)
+    .filter((item) => item.lastTime === '19900101');
 
   const limit = pLimit(100);
 
