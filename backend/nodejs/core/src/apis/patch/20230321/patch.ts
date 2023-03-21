fbff4b4b390f9aa542247be9bd0c2d5839665073
wwalpha <wwalpha@gmail.com>
2023-03-21 10:19:28 +0900

    add new patch

---
+++ backend/nodejs/core/src/apis/patch/20230321/patch.ts
@@ -0,0 +1,31 @@
+import { Environment } from '@consts';
+import { LearningService } from '@services';
+import { DateUtils, DBHelper } from '@utils';
+
+const patch = async (): Promise<void> => {
+  const userId = 'Google_109439805128280065775';
+
+  const learnings = await LearningService.listByUser(userId);
+  const now = DateUtils.getNow();
+
+  const targets = learnings
+    .filter((item) => item.subject_status !== undefined)
+    .map((item) => {
+      item.subject_status = undefined;
+      return item;
+    });
+
+  // clear status
+  await DBHelper().bulk(Environment.TABLE_NAME_LEARNING, targets);
+
+  const tests = learnings
+    .filter((item) => item.nextTime >= now)
+    .map((item) => {
+      item.subject_status = `${item.subject}_TEST`;
+      return item;
+    });
+
+  await DBHelper().bulk(Environment.TABLE_NAME_LEARNING, tests);
+};
+
+patch();
