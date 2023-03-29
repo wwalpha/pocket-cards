d365287c9c1605e9decf6dcc7457bba941a03651
wwalpha <wwalpha@gmail.com>
2023-03-29 11:27:37 +0900

    update

--- backend/nodejs/core/test/services/curriculums/regist.test.ts
+++ backend/nodejs/core/test/services/curriculums/regist.test.ts
@@ -32,7 +32,7 @@ describe('Curriculums', () => {
     ]);
   });
 
-  test.skip('Curriculums01:カリキュラム登録_普通', async () => {
+  test('Curriculums01:カリキュラム登録_普通', async () => {
     const apiPath = '/v1/curriculums';
 
     const res = await request(server)
@@ -55,29 +55,6 @@ describe('Curriculums', () => {
 
     expect(orderBy(learning, 'qid')).toMatchObject(CURRICULUMS.CURRI_001_EXPECT_01);
     expect(curriculum?.Item).toMatchObject(CURRICULUMS.CURRI_001_EXPECT_02);
-  });
-
-  test.skip('Curriculums02:カリキュラム登録_実力', async () => {
-    const apiPath = '/v1/curriculums';
-
-    const res = await request(server)
-      .post(apiPath)
-      .set('username', HEADER_GUARDIAN)
-      .send(CURRICULUMS.CURRI_002_REQ as APIs.CurriculumRegistRequest);
-
-    // status code
-    expect(res.statusCode).toBe(200);
-
-    const response = res.body as APIs.CurriculumRegistResponse;
-
-    const curriculum = await DBHelper().get({
-      TableName: Environment.TABLE_NAME_CURRICULUMS,
-      Key: {
-        id: response.id,
-      } as Tables.TCurriculumsKey,
-    });
-
-    expect(curriculum?.Item).toMatchObject(CURRICULUMS.CURRI_002_EXPECT_02);
   });
 
   test.skip('Curriculums03: Group Id not exists', async () => {
@@ -132,7 +109,7 @@ describe('Curriculums', () => {
     expect(res.text).toEqual('No questions in group');
   });
 
-  test.skip('Curriculums07: 英語初回登録', async () => {
+  test('Curriculums07: 英語登録、既存なし', async () => {
     const apiPath = '/v1/curriculums';
 
     const res = await request(server)
@@ -144,7 +121,6 @@ describe('Curriculums', () => {
 
     // status code
     expect(res.statusCode).toBe(200);
-
     const learning = await LearningService.listByUser(HEADER_USER, 'jzqKogQQw7nvZnrkrQaMWN');
     const userwords = await DBHelper().scan({
       TableName: Environment.TABLE_NAME_USER_WORDS,
