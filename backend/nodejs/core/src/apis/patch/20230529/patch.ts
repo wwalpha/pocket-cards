9f8b01a75c8ccf043b21d588896c9bb411e8879c
wwalpha <wwalpha@gmail.com>
2023-05-29 13:52:53 +0000

    update

--- backend/nodejs/core/src/apis/patch/20230529/patch.ts
+++ backend/nodejs/core/src/apis/patch/20230529/patch.ts
@@ -1,24 +1,25 @@
+import { Environment } from '@consts';
 import { LearningService } from '@services';
+import { DBHelper } from '@utils';
 
 const patch = async (): Promise<void> => {
   const userId = 'Google_109439805128280065775';
 
-  const [group1, group2, group3] = await Promise.all([
-    LearningService.listByUser(userId, 'xdCeUT337zFLUecrVLR7RV'),
+  const [learnings] = await Promise.all([
+    // LearningService.listByUser(userId, 'xdCeUT337zFLUecrVLR7RV'),
     LearningService.listByUser(userId, 'btWiokeG73MG5kzp1TT1KN'),
-    LearningService.listByUser(userId, 'iyPV3R4qYrLAFcpYRTgYUT'),
+    // LearningService.listByUser(userId, 'iyPV3R4qYrLAFcpYRTgYUT'),
   ]);
-  const learnings = [...group1, ...group2, ...group3];
 
   learnings.forEach((item) => {
     item.nextTime = '20230528';
     item.times = 3;
   });
 
-  await Promise.all(learnings.map((item) => LearningService.update(item)));
+  // await Promise.all(learnings.map((item) => LearningService.update(item)));
 
   // clear status
-  // await DBHelper().bulk(Environment.TABLE_NAME_LEARNING, learnings);
+  await DBHelper().bulk(Environment.TABLE_NAME_LEARNING, learnings);
 };
 
 patch();
