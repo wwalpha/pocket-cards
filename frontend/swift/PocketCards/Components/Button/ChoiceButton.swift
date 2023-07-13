be6d68f427e0849dc165cc0d0ac323f6bb20beed
wwalpha <wwalpha@gmail.com>
2023-07-13 10:29:06 +0900

    update

--- frontend/swift/PocketCards/Components/Button/ChoiceButton.swift
+++ frontend/swift/PocketCards/Components/Button/ChoiceButton.swift
@@ -11,23 +11,37 @@ struct ChoiceButton: View {
     var text: String
     var index: String
     var isError: Bool = false
+    var frameHeight: CGFloat = 0
+    var fontSize: CGFloat = 32
 
     let onDetail: () -> Void
+
+    init(text: String, index: String, isError: Bool, onDetail: @escaping () -> Void) {
+        self.init(text: text, index: index, isError: isError, fontSize: 32, frameHeight: 64, onDetail: onDetail)
+    }
+
+    init(text: String, index: String, isError: Bool, fontSize: CGFloat, frameHeight: CGFloat, onDetail: @escaping () -> Void) {
+        self.text = text
+        self.index = index
+        self.isError = isError
+        self.frameHeight = frameHeight
+        self.onDetail = onDetail
+        self.fontSize = fontSize
+    }
 
     var body: some View {
         HStack {
             Button(action: onDetail) {
                 HStack {
                     Text(self.index)
-                        .frame(width: 64, height: 64, alignment: .center)
-                        .font(.title)
+                        .frame(width: 64, height: frameHeight, alignment: .center)
+                        .font(.system(size: fontSize, design: .default))
                         .background(Color.purple)
                         .foregroundColor(Color.white)
                     Text(self.text)
                         .frame(maxWidth: .infinity, maxHeight: 32, alignment: .leading)
-                        .padding(.all)
                         .padding(.leading, 16)
-                        .font(.title)
+                        .font(.system(size: fontSize, design: .default))
                         .foregroundColor(Color.black)
 //                        .foregroundColor(Color.gray.opacity(0.8))
                         .contentShape(Rectangle())
