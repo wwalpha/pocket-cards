71f6fbd9a20641963f03049f52bc1dc852df83fc
wwalpha <wwalpha@gmail.com>
2024-12-30 03:17:59 +0000

    bugfix

--- backend/nodejs/auth_manager/src/app.ts
+++ backend/nodejs/auth_manager/src/app.ts
@@ -80,9 +80,9 @@ export const login = async (
     const accessToken = session.getAccessToken().getJwtToken();
     const refreshToken = session.getRefreshToken().getToken();
 
-    res.cookie(`pkc.${request.username}.idToken`, idToken, { httpOnly: true });
-    res.cookie(`pkc.${request.username}.accessToken`, accessToken, { httpOnly: true });
-    res.cookie(`pkc.${request.username}.refreshToken`, refreshToken, { httpOnly: true });
+    res.cookie(`pkc.${request.username.replace('@','')}.idToken`, idToken, { httpOnly: true });
+    res.cookie(`pkc.${request.username.replace('@','')}.accessToken`, accessToken, { httpOnly: true });
+    res.cookie(`pkc.${request.username.replace('@','')}.refreshToken`, refreshToken, { httpOnly: true });
 
     return {
       success: 'true',
